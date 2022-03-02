b19b28d7c278b9216a19375723fca03e3fb2d547
User <masta54@mail.ru>
2022-03-02 11:19:33 +0700

    create new method "commitOne"

---
+++ src/test/java/qa/guru/junit/CommitGit.java
@@ -0,0 +1,12 @@
+package qa.guru.junit;
+
+import org.junit.jupiter.api.*;
+
+public class CommitGit {
+
+    @Test
+    void commitOne () {
+        System.out.println("new Commit One");
+    }
+
+}
